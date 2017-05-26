7aca429409fe075064b450ebf7d12bdf2dea3529
Lani Asep Sutisna <lani.asep@lawencon.com>
2017-05-26 14:50:19 +0700

    Update File Burger King

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:3308');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
